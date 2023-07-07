d390e56db83f4be2900bd5b75e2b9d4b1c797f50
Technicalslayer <nshayes4@gmail.com>
2023-07-07 16:50:13 -0500

    Add rotation, rotation restrictions
    Added rotation restrictions where player faces in one of 8 directions using mouse pointer. I might rework to use keyboard, will definitely add support for gamepad.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float move_speed = 10f;
 
     private Vector2 movement = Vector2.zero;
+    private Vector2 facing = Vector2.up; //direction player is facing
+    private float rotation_angle = 0f;
 
     //components
     private Rigidbody2D rb;
@@ -21,12 +23,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        movement = Vector2.zero;
+        movement = Vector2.zero; //reset every frame, probably not necessary
+        //get input
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //facing = new Vector2(Input.GetAxisRaw("something"), Input.GetAxisRaw("something"));
+        facing = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        rotation_angle = Vector2.SignedAngle(Vector2.right, facing) - 90f;
+        //clamp rotation to steps of 45 degrees
+        rotation_angle += 360; //make positive
+        rotation_angle += 45 / 2; //back up half step
+        int wedge_number = (int)(rotation_angle / 45);
+        rotation_angle = wedge_number * 45;
+        //rotation_angle = rotation_angle - (rotation_angle % 45);
+        Debug.Log(rotation_angle);
     }
 
     private void FixedUpdate() {
         movement.Normalize();
         rb.velocity = movement * move_speed;
+        rb.rotation = rotation_angle;
+        //clamp rotation to steps of 45 degrees
+
     }
 }
