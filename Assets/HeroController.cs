8fb9bee419874e9dd121867016ab2e1ed77d467c
Technicalslayer <nshayes4@gmail.com>
2023-07-07 21:44:16 -0500

    Add pathfinding package. Add Player tag to player.
    Using an asset from the unity store.
    Handles most of the pathfinding, still have to create a way to find targets for Hero and Orcs, and refine physics of the movement.

--- Assets/HeroController.cs
+++ Assets/HeroController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour
 {
+    public float detectionRange = 10f; //how far to search for the player or orcs
+
+    private float searchTimer = 0f;
+    private float searchTimerMax = 2f; //time in seconds between searches for player/orcs
+    private Transform target; //target to make way towards
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,24 @@ public class HeroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    private void FixedUpdate() {
+        //search for any nearby targets in line of sight
+        RaycastHit2D[] results = new RaycastHit2D[10]; 
+        results = Physics2D.CircleCastAll(transform.position, detectionRange, Vector2.zero);
+
+        if (results.Length > 0) {
+            //sort array by distance (The function already sorts by distance)
+            //check for player or orcs in range, and set target
+            for (int i = 0; i < results.Length; i++) {
+                if (results[i].collider.tag == "Orc" || results[i].collider.tag == "Player") { //Orc takes priority over player
+                    target = results[i].transform;
+                    break;
+                }
+            }
+        }
+    }
+
+
 }
