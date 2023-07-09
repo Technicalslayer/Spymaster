4e7ed04d292ada2207054610e839b134367299bc
Technicalslayer <nshayes4@gmail.com>
2023-07-09 09:55:40 -0500

    Add Main Menu, Victory Screen

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void StartButtonPress() {
+        //start game
+        SceneManager.LoadScene(1); //first level
+    }
+
+    public void WhooHooButtonPress() {
+        //back to menu
+        SceneManager.LoadScene(0);
+    }
+}
