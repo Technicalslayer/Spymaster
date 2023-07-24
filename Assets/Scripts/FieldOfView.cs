48a093768ea95f7ba596bb28435ce42f57c8c9cd
Technicalslayer <nshayes4@gmail.com>
2023-07-24 14:32:17 -0500

    Rework Player FoV detection. Clean code.

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -97,5 +97,4 @@ public class FieldOfView : MonoBehaviour
             angle = _angle;
         }
     }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
 
     //public settings
     public float move_speed = 10f;
-    public float spy_range = 10f; //how far out can you spy on the hero
     public float spy_timer_max = 1f; //how many seconds before incrementing spy meter
     public int health_max = 1; //how many hits the player can receive before dying
     public LayerMask raycastLayer;
@@ -21,32 +20,22 @@ public class PlayerController : MonoBehaviour
     public int spy_progress = 0; //100 is a full meter
     private float spy_timer = 0f; //current timer progress
     private int health_current;
-    private bool lineOfSight; //no obstacles between player and hero
-    
 
     //[SerializeField] private int max_slidervalue = 10;
 
     //components
     private Rigidbody2D rb;
-    //private Collider2D spy_view;
     public Image spyMeterFillImage;
     public Transform heroT;
-    //private GameObject FoVSprite;
-    //For Spymeter
-    //[SerializeField] private Slider slider;
+    private FieldOfView fieldOfView;
 
     // Start is called before the first frame update
     void Start()
     {
         //get components
         rb = GetComponent<Rigidbody2D>();
-        //spy_view = rb.GetComponent<Collider2D>();
-
+        fieldOfView = GetComponentInChildren<FieldOfView>();
         health_current = health_max;
-
-        //slider.maxValue = max_slidervalue;
-        //slider.gameObject.SetActive(false);
-        //slider.interactable = false;
     }
 
     // Update is called once per frame
@@ -57,13 +46,6 @@ public class PlayerController : MonoBehaviour
         //facing = new Vector2(Input.GetAxisRaw("something"), Input.GetAxisRaw("something"));
         facing = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         rotation_angle = Vector2.SignedAngle(Vector2.up, facing);
-        //clamp rotation to steps of 45 degrees
-        //rotation_angle += 360; //make positive
-        //rotation_angle += (int)(45 / 2); //back up half step
-        //int wedge_number = (int)(rotation_angle / 45);
-        //rotation_angle = wedge_number * 45;
-        //rotation_angle = rotation_angle - (rotation_angle % 45);
-        //Debug.Log(rotation_angle);
     }
 
     private void FixedUpdate() {
@@ -71,63 +53,43 @@ public class PlayerController : MonoBehaviour
         rb.velocity = movement * move_speed;
         rb.rotation = rotation_angle;
 
-        //shapecast for spy FoV
-        //cast, hitting hero and obstacle layers
-        //if hit obstacle, ignore cast
         //if hit only hero or hero hit before obstacle, then count as success, start to fill up meter
         if (heroT) {
-            Vector2 dir = heroT.position - transform.position;
-            RaycastHit2D[] results = new RaycastHit2D[0];
-            //int hit_count = Physics2D.Raycast(dir, results, Mathf.Infinity);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, Mathf.Infinity, raycastLayer);
-            Debug.DrawRay(transform.position, dir, Color.red);
+            if (TargetInViewRange(heroT.position, "Hero")) {
+                IncrementSpymeter();
+            }
+        }
+    }
 
-            if (hit.collider.tag == "Hero") {
-                lineOfSight = true;
-            }
-            else {
-                lineOfSight = false;
-            }
-            //if (hit_count == 1 && results[0].collider.tag == "Hero") {
-            //    //only hero was hit, have a clear line of sight to hero
-            //    lineOfSight = true;
-            //}
-            //else {
-            //    lineOfSight = false;
-            //}
+    private bool TargetInViewRange(Vector3 targetPos, string targetTag){
+        bool inAngle;
+        bool inRange;
+        bool correctTag;
+
+        //calc angle
+        Vector2 dir = targetPos - transform.position;
+        float angleToTarget = Vector2.Angle(transform.up, dir);
+
+        //raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, fieldOfView.viewDistance, raycastLayer);
+        if(hit && hit.collider.tag == targetTag){
+            correctTag = true; //this implies no obstacles were in the way
         }
-        
-            //    //only hero was hit
-            //    IncrementSpymeter();
-            //}
-            //else if (hit_count >= 2) {
-            //    //see if hero was hit at all
-            //    for (int i = 0; i < hit_count; i++) {
-            //        if (results[i].collider.tag == "Hero") {
-            //            //check that hero is closer to player than all other hits
-            //            for (int j = 0; j < hit_count; j++) {
-            //                if (j == i) continue; //skip hero index
-            //                if ((results[j].transform.position - transform.position).magnitude < (results[i].transform.position - transform.position).magnitude) {
-            //                    //player is closer to obstacle than hero
-            //                    //so do nothing
-            //                    return;
-            //                }
-            //            }
-            //            IncrementSpymeter();
-            //        }
-            //    }
-            //}
+        else{
+            correctTag = false;
         }
+        Debug.DrawRay(transform.position, dir, Color.red);
+
+        inAngle = angleToTarget < fieldOfView.viewAngle / 2;
+        inRange = Vector2.Distance(transform.position, targetPos) < fieldOfView.viewDistance;
+
+        return correctTag && inAngle && inRange;
+    }
 
     /// <summary>
     /// Called when player has line of sight to hero
     /// </summary>
     private void IncrementSpymeter() {
-        if (!lineOfSight) {
-            //not a clear shot
-            //Debug.Log("Obstructed View");
-            return;
-        }
         //increment timer
         spy_timer += Time.fixedDeltaTime;
         if (spy_timer > spy_timer_max) {
@@ -144,9 +106,6 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("YOU WIN");
                 FindObjectOfType<LocalMapManager>().LoadLevel();
             }
-
-            //Update visuals
-            //UpdateSpymeter()
         }
     }
 
