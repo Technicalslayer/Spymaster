8c3cc2d1e92596925adbf47f5622b940ad734e4d
Technicalslayer <nshayes4@gmail.com>
2023-08-11 16:34:24 -0500

    Update OverworldPlayerController.cs

--- Assets/Scripts/OverworldPlayerController.cs
+++ Assets/Scripts/OverworldPlayerController.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 public class OverworldPlayerController : MonoBehaviour
 {
 
+    //public settings
     public float move_speed = 10f;
+    public int rotationDelayFramesMax = 5; //how many frames to prevent changing direction
 
+
+    //private settings
     private Vector2 movement = Vector2.zero;
     private Vector2 facing = Vector2.up; //direction player is facing
     private float rotation_angle = 0f;
+    private bool rotationDelay = false; //used to prevent snapping to the wrong direction when releasing input
+    private int rotationDelayFrames = 0;
+
 
     //components
     private Rigidbody2D rb;
+    public Image spyMeterFillImage;
 
     // Start is called before the first frame update
     void Start() {
@@ -23,19 +31,7 @@ public class OverworldPlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        movement = Vector2.zero; //reset every frame, probably not necessary
-        //get input
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //facing = new Vector2(Input.GetAxisRaw("something"), Input.GetAxisRaw("something"));
-        facing = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        rotation_angle = Vector2.SignedAngle(Vector2.right, facing) - 90f;
-        //clamp rotation to steps of 45 degrees
-        rotation_angle += 360; //make positive
-        rotation_angle += (int)(45 / 2); //back up half step
-        int wedge_number = (int)(rotation_angle / 45);
-        rotation_angle = wedge_number * 45;
-        //rotation_angle = rotation_angle - (rotation_angle % 45);
-        //Debug.Log(rotation_angle);
+        GetInput();
     }
 
     private void FixedUpdate() {
@@ -45,7 +41,36 @@ public class OverworldPlayerController : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-       
+    private void GetInput() {
+        //get input
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (!rotationDelay && (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))) {
+            //start timer to ignore key releases on next couple frames?
+            rotationDelay = true;
+        }
+
+        if (rotationDelay) {
+            //do not read movement input for rotation for a few frames to prevent snapping to wrong direction
+            rotationDelayFrames++;
+            if (rotationDelayFrames >= rotationDelayFramesMax) {
+                rotationDelay = false;
+                rotationDelayFrames = 0;
+            }
+        }
+        else if (movement.magnitude > 0) {
+            facing = movement;
+        }
+        rotation_angle = Vector2.SignedAngle(Vector2.up, facing);
     }
+
+    public void UpdateSpymeter(int spy_progress) {
+        spyMeterFillImage.fillAmount = (float)spy_progress / 100f; //total
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+    }
+
 }
